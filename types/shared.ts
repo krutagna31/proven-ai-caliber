2cce33207467249db7148bfeac0e7e9332af276b
Krutagna Patel <krutagnapatel31@gmail.com>
2025-12-31 09:51:35 +0530

    feat(types): add option and input types
    Introduces shared option and input types to enable reuse across projects. This removes duplication and ensures consistency in type definitions.

---
+++ types/shared.ts
@@ -0,0 +1,34 @@
+export type Option<T> = {
+  label: string;
+  value: T;
+};
+
+export type BaseInput<T extends string = string> = {
+  name: T;
+  label: string;
+  description: string;
+};
+
+export type FileInput<T extends string = string> = BaseInput<T> & {
+  id: string;
+  type: "file";
+  accept: string;
+};
+
+export type SelectInput<T extends string = string> = BaseInput<T> & {
+  id: string;
+  type: "select";
+  placeholder: string;
+  options: Option<string>[];
+};
+
+export type TextInput<T extends string = string> = BaseInput<T> & {
+  id: string;
+  placeholder: string;
+  type: "text" | "password" | "email" | "number";
+};
+
+export type FormInput<T extends string = string> =
+  | FileInput<T>
+  | SelectInput<T>
+  | TextInput<T>;
